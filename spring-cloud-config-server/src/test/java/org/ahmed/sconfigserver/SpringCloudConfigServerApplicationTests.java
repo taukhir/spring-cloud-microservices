3d6115c94c1a47a8f49ba4a6ea784eb77fde29f0
Ahmed <tauqeerahmed.hamaed@gmail.com>
2024-12-21 19:56:04 +0530

    Adding Changes

---
+++ spring-cloud-config-server/src/test/java/org/ahmed/sconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahmed.sconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
