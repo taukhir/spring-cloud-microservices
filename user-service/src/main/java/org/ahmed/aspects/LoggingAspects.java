e58fd927f909b2ce9ffeeb354c220563b98b468d
Ahmed <tauqeerahmed.hamaed@gmail.com>
2025-01-09 14:38:41 +0530

    AOP

--- user-service/src/main/java/org/ahmed/aspects/LoggingAspects.java
+++ user-service/src/main/java/org/ahmed/aspects/LoggingAspects.java
@@ -19,12 +19,5 @@ public class LoggingAspects {
     public void logAfter(JoinPoint joinPoint) {
         log.info("After method execution: {}", joinPoint.getSignature().getName());
     }
-//    @Pointcut("within(org.ahmed.service.*)")
-//    public void saveUserMethod() {
-//    }
-//    @Before("saveUserMethod()")
-//    public void logBeforeSaveUser(JoinPoint joinPoint) {
-//        log.info("Before saving user: {}", joinPoint.getSignature().getName());
-//    }
 
 }
