7b7aae331dadf06b7740f0178e6b83011bf39046
Ahmed <tauqeerahmed.hamaed@gmail.com>
2025-01-09 21:46:39 +0530

    Spring Security

---
+++ spring-cloud-security/src/main/java/org/ahmed/spring_cloud_security/SpringCloudSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.ahmed.spring_cloud_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSecurityApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-security/src/test/java/org/ahmed/spring_cloud_security/SpringCloudSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahmed.spring_cloud_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
